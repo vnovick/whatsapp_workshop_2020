ae6f45cda560460aa041f0096a68ef7f085b83ae
Vladimir Novick <vnovick@gmail.com>
2020-10-07 14:15:19 +0300

    adjust keyboard offset

--- src/screens/ChatViewScreen.js
+++ src/screens/ChatViewScreen.js
@@ -9,36 +9,39 @@ import {
 import {Message, Compose} from '../components';
 import ApplicationStyles from '../styles/appstyles';
 import {getMessagesById, postMessage} from '../services/api';
+import {HeaderHeightContext} from '@react-navigation/stack';
 
 export const ChatViewScreen = ({route}) => {
-  const {id} = route.params;
+  const {userId} = route.params;
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     const fetchMessages = async () => {
-      const result = await getMessagesById(id);
+      const result = await getMessagesById(userId);
       setMessages(result);
     };
     fetchMessages();
-  }, [id, messages]);
-  const keyboardVerticalOffset = Platform.OS === 'ios' ? 60 : 0;
-
+  }, [userId, messages]);
   return (
     <ImageBackground
       source={require('../assets/imgs/background.png')}
       style={styles.container}>
-      <KeyboardAvoidingView
-        style={styles.container}
-        behavior={Platform.OS === 'ios' ? 'padding' : null}
-        keyboardVerticalOffset={keyboardVerticalOffset}>
-        <FlatList
-          style={styles.container}
-          data={messages}
-          renderItem={({item}) => <Message key={item.id} {...item} />}
-          keyExtractor={(item, index) => `message-${index}`}
-        />
-        <Compose submit={postMessage} />
-      </KeyboardAvoidingView>
+      <HeaderHeightContext.Consumer>
+        {(headerHeight) => (
+          <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : null}
+            keyboardVerticalOffset={headerHeight}>
+            <FlatList
+              style={styles.container}
+              data={messages}
+              renderItem={({item}) => <Message key={item.id} {...item} />}
+              keyExtractor={(item, index) => `message-${index}`}
+            />
+            <Compose submit={postMessage} />
+          </KeyboardAvoidingView>
+        )}
+      </HeaderHeightContext.Consumer>
     </ImageBackground>
   );
 };
